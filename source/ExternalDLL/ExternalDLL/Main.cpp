19ca74994a76d2b0948f2be31ba71fab872a1ae3
vera98x <v.schoonderwoerd@outlook.com>
2019-02-11 11:47:25 +0100

    DLL created and inplementatieplan in working directory

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char * argv[]) {
 
 
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("D:\\Users\\Rolf\\Downloads\\TestA5.jpg", *input)) {
+	if (!ImageIO::loadImage("C:\\ti-software\\HU-Vision-Vera-Florian-V2C\\testsets\\Set A\\TestSet Images\\child-1.jpg", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
