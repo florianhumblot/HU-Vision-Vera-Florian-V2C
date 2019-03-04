d23d202f43d68954dec0a88218bc7da41c165b24
vera98x <v.schoonderwoerd@outlook.com>
2019-03-04 13:30:07 +0100

    preprocessing works

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -92,7 +92,7 @@ int main(int argc, char * argv[]) {
 bool executeSteps(DLLExecution * executor) {
 
 	//Execute the four Pre-processing steps
-	if (!executor->executePreProcessingStep1(false)) {
+	if (!executor->executePreProcessingStep1(true)) {
 		std::cout << "Pre-processing step 1 failed!" << std::endl;
 		return false;
 	}
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -2,9 +2,10 @@
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	IntensityImage * intensitiyImg = new IntensityImageStudent();
+	IntensityImage * intensitiyImg = new IntensityImageStudent(image.getWidth(), image.getHeight());
 	for (unsigned int i = 0; i < image.getHeight()*image.getWidth(); i++) {
-		int gray = 0.299 * image.getPixel(i).r + 0.587 * image.getPixel(i).g + 0.114 * image.getPixel(i).b;
+		RGB pixelRGB = image.getPixel(i);
+		int gray = 0.299 * pixelRGB.r + 0.587 * pixelRGB.g + 0.114 * pixelRGB.b;
 		Intensity intens = Intensity(gray);
 		intensitiyImg->setPixel(i, intens);
 	}
