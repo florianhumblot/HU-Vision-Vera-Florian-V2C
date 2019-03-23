64e5d7b1156a6fac5aa851b54dbe801246efdafa
vera98x <v.schoonderwoerd@outlook.com>
2019-03-23 14:59:04 +0100

    push for error

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -31,7 +31,7 @@ int main2(int argc, char * argv[]) {
 
 
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("../../../testsets/Set A/TestSet Images/female-3.png", *input)) {
+	if (!ImageIO::loadImage("../../../testsets/Set A/TestSet Images/male-1.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
